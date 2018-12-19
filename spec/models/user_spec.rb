3e8f090ac55c86e45dd7dc207c7f75f7cf4aa94f
Kirill Komarovich <still2405@gmail.com>
2018-12-19 16:18:32 +0300

    Add user spec. Add simplecov

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build(:user) }
+
+  it { is_expected.to have_many(:access_tokens) }
 end
