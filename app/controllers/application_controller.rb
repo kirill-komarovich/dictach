05d91ffe625d247939544fc1a830247268cac7bf
Kirill Komarovich <still2405@gmail.com>
2019-01-08 12:53:18 +0300

    Change authentication to cookie (#10)
    * Add cookies
    * Add session
    * Remove cors
    * Remove doorkeeper
    * Update webpack dev server version
    * Remove redunant specs
    * Add sign up specs
    * Correct user, sign in specs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+  include ActionController::Cookies
 
   before_action :authenticate_user!
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  use_doorkeeper do
-    skip_controllers :authorizations, :applications, :authorized_applications
-    controllers tokens: 'users/tokens'
-  end
   devise_for :users,
              defaults: { format: :json },
-             controllers: {
-               registrations: 'users/registrations'
-             },
-             skip: %i[sessions password]
+             skip: %i[password]
   get 'home', to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
