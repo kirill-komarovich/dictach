60bd9df3155387e333a3c8811b72fe473284f023
Kirill Komarovich <still2405@gmail.com>
2019-03-06 21:45:28 +0300

    Correct rubocop offences

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-
   scope :api do
     devise_for :users,
-              defaults: { format: :json },
-              skip: %i[password]
+               defaults: { format: :json },
+               skip: %i[password]
     resources :authentication_checks, only: :index, defaults: { format: :json }
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
